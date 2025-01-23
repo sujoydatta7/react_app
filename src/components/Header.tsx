d54bb3761b48238ed0971144aae7f651ca4f1910
sujoydatta7 <dattasujoy9@gmail.com>
2025-01-23 09:59:46 +0530

    commiting-basic-files

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+function Header() {
+  return (
+    <>
+      <header>
+        <h1>React Essentials</h1>
+        <p>
+          Fundamental React concepts you will need for almost any app you are
+          going to build!
+        </p>
+      </header>
+    </>
+  );
+}
+
+export default Header;
